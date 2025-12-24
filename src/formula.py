3a9db30933def1c0174cfff180252bb826f8fcac
Lamine SOW <lamine.2004pro.sow@gmail.com>
2025-12-24 13:27:36 +0100

    creation de chemistry_rules.py

---
+++ src/formula.py
@@ -0,0 +1,47 @@
+class Hydrocabon:
+    pass
+
+class Alkane(Hydrocabon):
+    pass
+
+class Alkene(Hydrocabon):
+    pass
+
+class Alkyne(Hydrocabon):
+    pass
+
+class Aromatic(Hydrocabon):
+    pass
+
+#================================================
+
+class Alcohol(Hydrocabon):
+    pass
+
+class Ether(Hydrocabon):
+    pass
+
+class Phenol(Hydrocabon):
+    pass
+
+#================================================
+
+class Amine(Hydrocabon):
+    pass
+
+class Amide(Hydrocabon):
+    pass
+
+class Cetone(Hydrocabon):
+    pass
+
+#================================================
+
+class CarboxylicAcid(Hydrocabon):
+    pass
+
+class CarboxylicAcidDerivative(Hydrocabon):
+    pass
+
+#================================================
+
